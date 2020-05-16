7d0e3b6f7cdeda59aa2b2e8062876a8f17a74b84
edlopen <eduardo.lopez.penalver@gmail.com>
2020-05-16 12:41:07 +0200

    [ADD] kata 3

---
+++ Kata3/main.py
@@ -0,0 +1,8 @@
+age = int(input('Introduce tu edad: 200'))
+
+if age < 4:
+    print('El precio de la entrada es 0.')
+elif age <= 18:
+    print('El precio de la entrada es 5.')
+else:
+    print('El precio de la entrada es 10.')
